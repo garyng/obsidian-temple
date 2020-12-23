76fbd48f6498a1d40b0506103e314c85bd07cd38
garyng <garyng.zhongbo@gmail.com>
2020-12-23 22:52:40 +0800

    refactor: strongly type aggregated contexts

--- src/TempleService.ts
+++ src/TempleService.ts
@@ -3,26 +3,24 @@ import * as _ from 'lodash';
 import { ITempleProvider } from './providers/ITempleProvider';
 import { TempleContext } from './providers/TempleContext';
 
+export interface IAggregatedContext {
+	[name: string]: TempleContext<any>;
+}
+
 export class TempleService {
-	// todo: discover all variables provider
-	// todo: render templates
-	// todo: output to an object instead 
-	// {
-	// 	newContent: "xxx",
-	// }
 	private _providers: ITempleProvider<any>[] = [];
 
 	register(provider: ITempleProvider<any>): void {
 		this._providers.push(provider);
 	}
 
-	resolve(): { [name: string]: TempleContext<any>; } {
+	resolve(): IAggregatedContext {
 		let contexts = this._providers.map(provider => ({ [provider.name]: provider.provide() }));
 		return Object.assign({}, ...contexts);
 	}
 
-	render(template: string): string {
-		let aggregated = this.resolve();
+	render(template: string, aggregated: IAggregatedContext | null = null): string {
+		aggregated ??= this.resolve();
 		// only take the context from each TempleContext for nunjucks
 		let context = _.mapValues(aggregated, context => context.context);
 		return njk.renderString(template, context);
