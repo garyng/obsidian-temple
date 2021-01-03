b3c5b664348ba8e7aac01f706aa867fcaf97c346
garyng <garyng.zhongbo@gmail.com>
2021-01-03 21:32:08 +0800

    feat: add docs for each provider

--- src/providers/ClipboardTempleProvider.ts
+++ src/providers/ClipboardTempleProvider.ts
@@ -3,10 +3,33 @@ import { ITempleProvider } from './ITempleProvider';
 import { TempleContext } from './TempleContext';
 import { ClipboardContext } from './ClipboardContext';
 import { injectable } from 'tsyringe';
+import { TempleDocsContext } from './TempleDocsContext';
 
 @injectable()
 export class ClipboardTempleProvider implements ITempleProvider<ClipboardContext> {
 	name = 'clipboard';
+
+	async docs(): Promise<TempleDocsContext<ClipboardContext>> {
+		return {
+			context: await this.provide(),
+			template: `
+# \`clipboard\`
+Extracts data from your system clipboard. Uses [sindresorhus/clipboardy](https://github.com/sindresorhus/clipboardy).
+
+## Usages
+
+{% raw %}\`\`\`
+text: {{ clipboard.text }}
+\`\`\`{% endraw %}
+
+outputs:
+
+\`\`\`
+text: {{ clipboard.text }}
+\`\`\`
+`,
+		}
+	}
 
 	async provide(): Promise<TempleContext<ClipboardContext>> {
 		const text = await read();
--- src/providers/FileInfoTempleProvider.ts
+++ src/providers/FileInfoTempleProvider.ts
@@ -3,6 +3,7 @@ import { ITempleProvider } from './ITempleProvider';
 import { FileInfoContext } from './FileInfoContext';
 import { TempleContext } from './TempleContext';
 import { injectable } from 'tsyringe';
+import { TempleDocsContext } from './TempleDocsContext';
 
 @injectable()
 export class FileInfoTempleProvider implements ITempleProvider<FileInfoContext> {
@@ -10,6 +11,35 @@ export class FileInfoTempleProvider implements ITempleProvider<FileInfoContext> 
 
 	constructor(private _workspace: Workspace) {
 	}
+	
+    async docs(): Promise<TempleDocsContext<FileInfoContext>> {
+        return {
+            context: await this.provide(),
+			template: `
+# \`file\`
+
+Exposes Obsidian's internal [\`TFile\`](https://github.com/obsidianmd/obsidian-api/blob/d10f2f6efc0d0d7c9bf96cd435ef376b18fbd6d8/obsidian.d.ts#L2206) structure for templating.
+
+## Usages
+
+{% raw %}\`\`\`
+path: {{ file.path }}
+name: {{ file.name }}
+basename: {{ file.basename }}
+extension: {{ file.extension }}
+\`\`\`{% endraw %}
+
+outputs:
+
+\`\`\`
+path: {{ file.path }}
+name: {{ file.name }}
+basename: {{ file.basename }}
+extension: {{ file.extension }}
+\`\`\`
+`,
+        }
+    }
 
 	async provide(): Promise<TempleContext<FileInfoContext>> {
 		const file = this._workspace.getActiveFile();
