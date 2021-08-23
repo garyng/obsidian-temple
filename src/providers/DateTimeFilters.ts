a1df89b6e4ae50b949a30e33f09de6d36124bd1e
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2021-08-23 13:03:46 +0200

    🐛 (formatDate): parameter is now optional
    TODO: Add setting for default time format

--- src/providers/DateTimeFilters.ts
+++ src/providers/DateTimeFilters.ts
@@ -1,14 +1,18 @@
-import { injectable } from 'tsyringe';
 import { DateTime } from 'luxon';
+import { injectable } from 'tsyringe';
 import { DateTimeProvider } from './DateTimeProvider';
 
 @injectable()
 export class DateTimeFilters {
 
-	constructor(private _datetime: DateTimeProvider) {
-	}
+    constructor(private _datetime: DateTimeProvider) {
+    }
 
-	public format(input: DateTime, format: string): string {
-		return this._datetime.apply(input).toFormat(format);
-	}
+    public format(input: DateTime, format: string | null = null): string {
+        if (format === null) {
+            // TODO: Add setting for default time format (here iso8601)
+            format = "yyyy-LL-dd HH:mm:ss ZZZZ";
+        }
+        return this._datetime.apply(input).toFormat(format);
+    }
 }
