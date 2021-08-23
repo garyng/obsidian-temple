9fd8268e52ede9f9a59cb65b56e940eb9e9ef36b
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2021-08-23 12:55:53 +0200

    🐛 (DateTime): Add implicite cast for Date and number (fix #3)

--- src/providers/DateTimeProvider.ts
+++ src/providers/DateTimeProvider.ts
@@ -1,27 +1,45 @@
-import { TempleSettings } from '../settings/TempleSettings';
-import { inject, injectable } from 'tsyringe';
-import { Symbols } from '../Symbols';
 import { DateTime } from 'luxon';
+import { inject, injectable } from 'tsyringe';
+import { TempleSettings } from '../settings/TempleSettings';
+import { Symbols } from '../Symbols';
 
 
 @injectable()
 export class DateTimeProvider {
-	constructor(@inject(Symbols.TempleSettings) private _settings: TempleSettings) { }
+    constructor(@inject(Symbols.TempleSettings) private _settings: TempleSettings) { }
 
-	public now(): DateTime {
-		return this.apply(DateTime.local());
-	}
+    public now(): DateTime {
+        return this.apply(DateTime.local());
+    }
 
-	/**
-	 * Apply locale and timezone settings
-	 */
-	public apply(dt: DateTime): DateTime {
-		if (this._settings.datetime.locale) {
-			dt = dt.setLocale(this._settings.datetime.locale);
-		}
-		if (this._settings.datetime.timezone) {
-			dt = dt.setZone(this._settings.datetime.timezone);
-		}
-		return dt;
-	}
+    /**
+     * Apply locale and timezone settings, also cast JS Date (returned from other APIs)
+     *
+     * Since apply is called by all filters that return a DateTime to ensure that
+     * settings are respected all implicit conversion and maniputlations should be
+     * concentrated here.
+     */
+    public apply(dt: DateTime | Date | number): DateTime {
+        // Type verification and casting
+        if (typeof dt == "number") {
+            dt = DateTime.fromMillis(dt as number);
+        } else if (dt instanceof Date) {
+            dt = DateTime.fromJSDate(dt as Date);
+        }
+        // console.log(dt)
+        // } else if (!(dt instanceof DateTime)) {
+        //     console.error("Rejected DateTime value:", dt)
+        //     throw TypeError("Only DateTime, Date and ints are accepted for date filters");
+        // }
+
+        // Apply localization settings
+        if (this._settings.datetime.locale) {
+            dt = dt.setLocale(this._settings.datetime.locale);
+        }
+        if (this._settings.datetime.timezone) {
+            dt = dt.setZone(this._settings.datetime.timezone);
+        }
+
+        return dt;
+    }
 }
